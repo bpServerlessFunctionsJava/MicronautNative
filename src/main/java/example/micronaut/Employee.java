21c8d6e1d6ea45fe4d03e539ff6217064024aa5a
Robbe <robbe.dewintere@student.hogent.be>
2021-04-10 15:27:45 +0200

    Type id aanpassen

--- src/main/java/example/micronaut/Employee.java
+++ src/main/java/example/micronaut/Employee.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String name;
     private String email;
     @ManyToOne
@@ -19,18 +19,18 @@ public class Employee {
     public Employee() {
     }
 
-    public Employee(int empId, String name, String email, Employee supervisor) {
+    public Employee(Integer empId, String name, String email, Employee supervisor) {
         this.id = empId;
         this.name = name;
         this.email = email;
         this.supervisor = supervisor;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
