f8a71c03c4fdd5be955592187c43de46a5be6460
Robbe <robbe.dewintere@student.hogent.be>
2021-04-23 20:22:54 +0200

    Change Input class

--- src/main/java/example/micronaut/Input.java
+++ src/main/java/example/micronaut/Input.java
@@ -1,5 +1,16 @@
 package example.micronaut;
 
+import io.micronaut.core.annotation.Introspected;
+
+@Introspected
 public class Input {
     String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
 }
